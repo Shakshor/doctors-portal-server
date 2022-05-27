df30099679d0de990308bdaf1368a03b2e4930c2
Shakshor <shakshor584@gmail.com>
2022-05-27 12:41:09 +0600

    create booking data and find existing treatement per day

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
 
         app.get('/service', async (req, res) => {
             const query = {}
@@ -27,6 +28,32 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        /**
+         * API Naming Convention
+         * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+         * app.get('/booking/:id') // get a specific booking
+         * app.post('/booking') // add a new booking
+         * app.patch('/booking/:id') // update a specific booking
+         * app.delete('/booking/:id') // delete a specific booking
+        */
+
+        // POST
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            // find the existing treatment 
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollection.insertOne(booking);
+            return res.send({ success: true, result });
+        });
+
+
+
+
     }
     finally {
 
